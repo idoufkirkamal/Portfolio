5043874457de67af7079a60d82b3a5da1029d9e8
IDOUFKIR Kamal <idoufkir.kamal@gmail.com>
2024-10-28 22:07:50 +0100

    Test something!

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-          hello world!
+          hello world!!!!
       </main>
     </>
   )
